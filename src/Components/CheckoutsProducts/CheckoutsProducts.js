9c7429147ab990c7b85dd07028490701d2f8adb9
chitminthu <chitminthu72745@gmail.com>
2021-10-06 14:30:07 +0630

    POS commit

--- src/Components/CheckoutsProducts/CheckoutsProducts.js
+++ src/Components/CheckoutsProducts/CheckoutsProducts.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import SingleCheckout from "./SingleCheckout";
 const CheckoutsProducts = (props) => {
+    let total = 0;
+    const [totalPrice,setTotalPrice] = useState(props.price);
+    const [payPrice,setPayPrice] = useState(0);
+    const priceall = (ppp) => {
+        setTotalPrice(
+            total += ppp.price
+        );
+    }
+
+    const payment = (e) => {
+        if(totalPrice < e.target.value){
+            setPayPrice(e.target.value - totalPrice);
+        }else{
+            setPayPrice(0);
+        }
+    }
     return(
         <div className="product_table_row">
             <div className="product_table_list">
                 {
                     props.checkOutProducts.map((checkProduct)=>{
                         return(
-                            <SingleCheckout key={checkProduct.id} stock={checkProduct.stock} title={checkProduct.title} price={checkProduct.price} />
+                            <SingleCheckout key={checkProduct.id} stock={checkProduct.stock} title={checkProduct.title} price={checkProduct.price} priceall={priceall} />
                         )
                     })
                 }
@@ -17,7 +33,7 @@ const CheckoutsProducts = (props) => {
                     <tbody>
                         <tr>
                             <th>Sub Total</th>
-                            <td>10000 MMK</td>
+                            <td>Ks {totalPrice}</td>
                         </tr>
                         <tr>
                             <th>Advance Payment</th>
@@ -26,13 +42,14 @@ const CheckoutsProducts = (props) => {
                                     <input
                                         type="text"
                                         className="customer_table_payment_input"
+                                        onChange={payment}
                                     />
                                 </div>
                             </td>
                         </tr>
                         <tr>
                             <th>Total</th>
-                            <td>100000 MMK</td>
+                            <td>Ks {payPrice}</td>
                         </tr>
                     </tbody>
                 </table>
--- src/Components/CheckoutsProducts/SingleCheckout.js
+++ src/Components/CheckoutsProducts/SingleCheckout.js
@@ -1,18 +1,17 @@
-import React from "react";
-const SingleCheckout = (props) => {
-    let qty = 1;
+import React ,{useState} from "react";
+const SingleCheckout = (props) => {    
     let stock = props.stock;
-    const stockChange = (e) => {
-        console.log(e.target.value);
+    const [enterAmount,subEnderAmount] = useState(stock);
+    const amountChangeHandler = (event) =>{
+        subEnderAmount(event.target.value);
+    };
+    const plus = () => { subEnderAmount(enterAmount + 1); }
+    const minus = () => { if(enterAmount > 1){ subEnderAmount(enterAmount - 1); }}
+    const pricealls = {
+        price : props.price * enterAmount
     }
-    const minus = () =>{
-        //
-    }
+    props.priceall(pricealls);
 
-    const plus = () => {
-        stock += qty;
-    }
-    
     return(
         <div className="card">
             <div className="row center">
@@ -22,26 +21,26 @@ const SingleCheckout = (props) => {
                 <div className="col-1 p-0">
                     <form action="">
                         <div className="control">
-                            <button type="button" className="bttn bttn-left" id="minus" onClick={minus}>
+                            <button type="button" className="bttn bttn-left" onClick={minus}>
                                 <span>-</span>
                             </button>
                             <input
                                 type="number"
                                 className="input"
                                 id="input"
-                                value={stock}
-                                onChange={stockChange}
+                                min="1" 
+                                step="1"
+                                value={enterAmount}
+                                onChange={amountChangeHandler}
                             />
-                            
-                            
-                            <button type="button" className="bttn bttn-right" id="plus" onClick={plus}>
+                            <button type="button" className="bttn bttn-right" onClick={plus}>
                                 <span>+</span>
                             </button>
                         </div>
                     </form>
                 </div>
                 <div className="col-1 p-0">
-                    <div className="product_price">{props.price * props.stock} MMK</div>
+                    <div className="product_price">{props.price * enterAmount} MMK</div>
                 </div>
                 <div className="col-1 p-0">
                     <button className="del">x</button>
